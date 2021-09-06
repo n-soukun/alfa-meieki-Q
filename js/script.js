c670c4d02e14a46052e7f6a6d648550ad0469937
soukun <soushi_mizuno@outlook.jp>
2021-09-06 23:18:59 +0900

    デザイン変更
    ファビコンを追加しました。ローディングアニメーションを追加しました。

--- js/script.js
+++ js/script.js
@@ -1,21 +1,36 @@
 $(function(){
+	ScrollReveal().reveal('.top_img', { delay: 240, distance: "40px",origin: "bottom", duration: 1400});
     ScrollReveal().reveal('.headline', { delay: 500, duration: 1000});
     ScrollReveal().reveal('.box_left', { delay: 500, distance: "20px", origin: "right", duration: 800});
     ScrollReveal().reveal('.box_right', { delay: 500, distance: "20px", origin: "left", duration: 800});
 
     let headNav = $("header");
 
-    $(window).on('load scroll', function () {
+	$(window).on("load", function() {
+		setTimeout(finish_loading, 1000);
+		move_header();
+	});
+
+    $(window).on("scroll", function () {
+		move_header();
+	});
+
+	function finish_loading(){
+		$(".loading").fadeOut();
+		$("body").css("overflow", "initial");
+	}
+
+	function move_header(){
 		//現在の位置が500px以上かつ、クラスfixedが付与されていない時
 		if($(this).scrollTop() > 500 && headNav.hasClass('fixed') == false) {
 			//位置を0に設定し、アニメーションのスピードを指定
-            headNav.addClass("fixed");
+			headNav.addClass("fixed");
 			headNav.animate({"top": 0},600);
 		}
 		//現在の位置が300px以下かつ、クラスfixedが付与されている時にfixedを外す
 		else if($(this).scrollTop() < 500 && headNav.hasClass('fixed') == true){
 			headNav.removeClass("fixed");
-            headNav.animate({"top": "-100px"},300);
+			headNav.animate({"top": "-100px"},300);
 		}
-	});
+	}
 });
