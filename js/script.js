10e336243689157dbb518fc4f64ff2ac146c1f27
soukun <soushi_mizuno@outlook.jp>
2021-09-06 16:23:12 +0900

    新しいトップページを作成

---
+++ js/script.js
@@ -0,0 +1,5 @@
+$(function(){
+    ScrollReveal().reveal('.headline', { delay: 500, duration: 1000});
+    ScrollReveal().reveal('.box_left', { delay: 500, distance: "20px", origin: "right", duration: 800});
+    ScrollReveal().reveal('.box_right', { delay: 500, distance: "20px", origin: "left", duration: 800});
+});
