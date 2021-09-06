f0c537d8738806b3404684fe1ed074c90504fa8c
soukun <soushi_mizuno@outlook.jp>
2021-09-06 22:11:32 +0900

    ヘッダーの仕様を変更
    チーム名を上部に固定し、ゲームロゴをヘッダーに追加しました。ヘッダーは追従するようにしました。

--- js/script.js
+++ js/script.js
@@ -2,4 +2,20 @@ $(function(){
     ScrollReveal().reveal('.headline', { delay: 500, duration: 1000});
     ScrollReveal().reveal('.box_left', { delay: 500, distance: "20px", origin: "right", duration: 800});
     ScrollReveal().reveal('.box_right', { delay: 500, distance: "20px", origin: "left", duration: 800});
+
+    let headNav = $("header");
+
+    $(window).on('load scroll', function () {
+		//現在の位置が500px以上かつ、クラスfixedが付与されていない時
+		if($(this).scrollTop() > 500 && headNav.hasClass('fixed') == false) {
+			//位置を0に設定し、アニメーションのスピードを指定
+            headNav.addClass("fixed");
+			headNav.animate({"top": 0},600);
+		}
+		//現在の位置が300px以下かつ、クラスfixedが付与されている時にfixedを外す
+		else if($(this).scrollTop() < 500 && headNav.hasClass('fixed') == true){
+			headNav.removeClass("fixed");
+            headNav.animate({"top": "-100px"},300);
+		}
+	});
 });
